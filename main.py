95e307251c4be7be4d61b9b81254f6dbda0f5f0a
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-02-19 03:10:27 +0100

    Essayer un exemple de regression logistique de PySpark MLlib avec un csv

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import SparkSession
 
 
@@ -5,15 +7,26 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Spark").getOrCreate()
 
 # Charger un fichier CSV en DataFrame
-df = spark.read.csv("data.csv", header=True, inferSchema=True) 
+df = spark.read.csv("ml_data.csv", header=True, inferSchema=True) 
 
 ''' Assurez-vous que le fichier 
 data.csv est dans le même répertoire que ce script ou fournissez le chemin complet 
 sinon on obtient une erreur : pyspark.errors.exceptions.captured.AnalysisException '''
 
+# Préparer les données
+assembler = VectorAssembler(inputCols=["feature1", "feature2"], outputCol="features")
+data = assembler.transform(df)
+
+# Entraîner un modèle de régression logistique
+lr = LogisticRegression(featuresCol="features", labelCol="label")
+model = lr.fit(data)
+
+predictions = model.transform(data)
+predictions.select("features", "label", "prediction",'probability').show()
+
 # Exécuter une requête SQL
-df.createOrReplaceTempView("data")
-result = spark.sql("SELECT region, AVG(revenue) FROM data GROUP BY region")
+""" df.createOrReplaceTempView("data")
+result = spark.sql("SELECT region, AVG(revenue) FROM data GROUP BY region") """
 
 '''En cette requête, nous sélectionnons la région et la moyenne des revenus, 
 puis nous groupons les résultats par région. Si votre fichier CSV ne contient pas
@@ -24,6 +37,6 @@ pyspark.errors.exceptions.captured.AnalysisException .
 '''
 
 # Afficher le résultat
-result.show()
+""" result.show() """
 
 input("Appuyez sur Entrée pour quitter...")
